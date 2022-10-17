e161ac29db472a704a23759c27056b7f31440bc2
Dhanur Sharma <dhanur@nbp-157-193.nbp.ruw.rutgers.edu>
2022-10-17 11:50:28 -0400

    Created class for predator

--- prey.py
+++ prey.py
@@ -8,6 +8,3 @@ class Prey:
         list_to_choose_from = arena[self.curr_pos]
         list_to_choose_from.append(self.curr_pos)
         self.curr_pos = random.choice(list_to_choose_from)
-
-class Predator:
-    pass
